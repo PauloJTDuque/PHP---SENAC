79a78ec4cdcbfd2597e45ddeb4d43a1d30f55547
PAULO JOSE TERRA DUQUE <paulo.jtduque@senacsp.edu.br>
2022-07-29 22:10:14 -0300

    aula 29207A

--- aula_07/index.php
+++ aula_07/index.php
@@ -1,13 +1,26 @@
 <?PHP
 
-$conex = mysqli_connect("localhost:3316","root","");
-$banco = mysqli_select_db($conex,"banco_teste");
-if ($banco){
-    echo "Conexão Estabelecida";
-}else{
-    echo "Conexão não Estabelecida";
-};
-$sql = "INSERT INTO `usuario` (`id`, `nome`, `email`) VALUES (NULL, 'Daffini Duque', 'daffini@terra.com.br')";  
-mysqli_query($conex,$sql);
+include 'conex.php';
+
+$conex->set_charset("utf8");
+
+// if ($banco){
+//     echo "Conexão Estabelecida";
+// }else{
+//     echo "Conexão não Estabelecida";
+// };
+//$sql = "INSERT INTO `usuario` (`id`, `nome`, `email`) VALUES (NULL, 'zé Duque', 'zé@terra.com.br')";  
+//mysqli_query($conex,$sql);
+
+$sql2 = "SELECT * FROM `usuario`";  // * = tudo
+$data = mysqli_query($conex, $sql2);
+
+echo"<br>";
+
+while ($res = mysqli_fetch_array($data)) {
+    echo "ID: ". $res[0] . "Nome: " . $res[1] . "Email: " . $res[2] . "<br>";
+}
+
+// Frameworks PHP: Laravel, CakePHP , Codelgniter
 
 ?>
