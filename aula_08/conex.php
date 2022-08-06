6cf4b67341a1b4b50588212531bcdef99750dc45
PAULO JOSE TERRA DUQUE <paulo.jtduque@senacsp.edu.br>
2022-08-05 21:37:49 -0300

    aula 0508

---
+++ aula_08/conex.php
@@ -0,0 +1,4 @@
+<?PHP
+    $conex = mysqli_connect("localhost:3316","root","");
+    $banco = mysqli_select_db($conex,"banco_teste");
+?>  
---
+++ aula_08/index3.php
@@ -0,0 +1,18 @@
+<?PHP
+
+include 'conex.php';
+
+$conex->set_charset("utf8");  // Definindo o charset
+
+$sql2 = "SELECT id,nome,email FROM `usuario` WHERE nome = 'Paulo Duque'";  
+$data = mysqli_query($conex, $sql2);
+
+echo"<br>";
+
+while ($res = mysqli_fetch_array($data)) {
+    echo "ID: ". $res[0] . "Nome: " . $res[1] . "Email: " . $res[2] . "<br>";
+}
+
+// Frameworks PHP: Laravel, CakePHP , Codelgniter
+
+?>
